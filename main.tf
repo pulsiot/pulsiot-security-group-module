97cafce9da868d5f218cd7ac7af6440c780434a1
debuggerboy <debuggerboy@gmail.com>
2022-05-03 02:03:52 +0530

    baseline config

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "sec_group" {
+  name        = "Pulsiot EC2 SG1"
+  description = "Primary Security Group used by Pulsiot EC2 instances"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group_rule" "http_allow" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = var.cidr_blocks
+  ipv6_cidr_blocks  = var.ipv6_cidr_blocks
+  security_group_id = aws_security_group.sec_group.id
+}
+
+resource "aws_security_group_rule" "ssh_allow" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = var.cidr_blocks
+  ipv6_cidr_blocks  = var.ipv6_cidr_blocks
+  security_group_id = aws_security_group.sec_group.id
+}
+
+resource "aws_security_group_rule" "allow_all" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  security_group_id = aws_security_group.sec_group.id
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "vpc_id" {
+  description = "Enter the VPC ID:"
+  type        = string 
+  default     = "vpc-00312478" 
+}
+
+
+variable "cidr_blocks" {
+  description = "Enter the IPv4 CIDR block:"
+  type        = list
+  default     = [ "0.0.0.0/0" ]
+}
+
+variable "ipv6_cidr_blocks" {
+  description = "Enter the IPv6 CIDR block:"
+  type        = list
+  default     = [ "::/0" ]
+}
