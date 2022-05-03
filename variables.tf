1be670fa84c00f9016f0f7cb34527fcbdf71b4d5
debuggerboy <debuggerboy@gmail.com>
2022-05-03 06:53:01 +0530

    updated variables

--- variables.tf
+++ variables.tf
@@ -8,11 +8,11 @@ variable "vpc_id" {
 variable "cidr_blocks" {
   description = "Enter the IPv4 CIDR block:"
   type        = list
-  default     = [ "0.0.0.0/0" ]
+  default     = ["0.0.0.0/0"]
 }
 
 variable "ipv6_cidr_blocks" {
   description = "Enter the IPv6 CIDR block:"
   type        = list
-  default     = [ "::/0" ]
+  default     = ["::/0"]
 }
