8a52c587c4a05447a0fc99bb5e5dff79f01bcf1d
debuggerboy <debuggerboy@gmail.com>
2022-05-08 06:34:18 +0530

    updates

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group_rule" "ipv4_ingress_allow" {
   from_port = each.value.from_port
   to_port = each.value.to_port
   protocol = each.value.protocol
-  cidr_blocks = each.value.cidr_blocks
+  cidr_blocks = [ each.value.cidr_blocks ]
   description = each.value.description
   security_group_id = aws_security_group.sec_group.id
 }
